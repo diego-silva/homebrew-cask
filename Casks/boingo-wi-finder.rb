e049a304d2fa2d21c3b9c564403225758f41225a
Diego Silva <diego.silva@live.com>
2019-07-10 23:49:10 -0300

    add Boingo Wi-Finder

---
+++ Casks/boingo-wi-finder.rb
@@ -0,0 +1,10 @@
+cask 'boingo-wi-finder' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://static-assets.boingo.com/apps/macupg003/wifinder-mac10-7.dmg'
+  name 'Boingo Wi-Finder'
+  homepage 'http://www.boingo.com/retail/boingo-wi-finder-for-macintosh/'
+
+  app 'Boingo Wi-Finder.app'
+end
